5de4157de219a40921cf84d87d332aa4eb4e1e6d
FollowMe <nareshpoudel126@gmail.com>
2023-05-04 08:10:09 +0545

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldCotroller.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+
+       
+    {
+        public IActionResult Index()
+             
+        {
+            DogViewModel Doggo = new DogViewModel()
+            { Name = "Laika", Age = 15 };
+            return View(Doggo);
+        }
+        public IActionResult Hello()
+        {
+            return View();
+        }
+
+    }
+}
